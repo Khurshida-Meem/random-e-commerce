6f274d2b3b8ce2cb1a91a1cf9eae9e4194967018
Khurshida-Meem <meemrumi59@gmail.com>
2022-05-17 22:42:45 +0600

    added dummy json data

---
+++ src/components/Navbar/BottomNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BottomNav = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BottomNav;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopNav from './TopNav';
+
+const Navbar = () => {
+    return (
+        <>
+            <TopNav />
+        </>
+    );
+};
+
+export default Navbar;
