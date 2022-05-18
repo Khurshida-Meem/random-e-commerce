e526c2d4ec0eacf9490fa6ab86492f7b5385fe9b
Khurshida-Meem <meemrumi59@gmail.com>
2022-05-18 10:28:38 +0600

    added logo and search bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
--- src/components/Navbar/TopNav.js
+++ src/components/Navbar/TopNav.js
@@ -1,10 +1,61 @@
+import { Box, Container } from '@mui/material';
 import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import logo from '../../assets/logo.svg'
+import { Flex } from '../../Element.style';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+
+// search field style
+const inputStyle = {
+    outline: 'none',
+    border: '0px',
+    borderLeft: '2px solid #6c757d',
+    padding: '0 8px',
+    marginLeft: '4px',
+    fontSize: '16px'
+}
+
+// search container
+const searchContainer = {
+    border: '2px solid #BCE3C9',
+    padding: '5px 20px',
+    borderRadius: '4px'
+}
+
+// badge style
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      right: -3,
+      top: 13,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: '0 4px',
+    },
+  }));
 
 const TopNav = () => {
     return (
-        <div>
-            
-        </div>
+        <Container sx={{ mt: '20px' }}>
+            <Flex justify='space-evenly'>
+                <img src={logo} alt="" height='60px' />
+
+                {/* =================== search field ======================== */}
+                <Box style={searchContainer} sx={{ display: { xs: 'none', md: 'block' } }}>
+                    <Flex>
+                        <SearchIcon sx={{ color: '#6c757d', fontSize: '26px' }} />
+                        <input style={inputStyle} type="text" placeholder='Search for Items' />
+                    </Flex>
+                </Box>
+
+                {/* ==================== badges ===================== */}
+                <Box>
+
+                </Box>
+            </Flex>
+        </Container>
     );
 };
 
