aefbc67b67e1defe81994ab7cbc71fef8b905397
Khurshida-Meem <meemrumi59@gmail.com>
2022-05-18 17:32:11 +0600

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import Navbar from './components/Navbar/Navbar';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home/Home';
+// import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+<Home />
+          </Route>
+        </Switch>
+      </Router>
+      
     </>
   );
 }
 
 export default App;
+
+// Tech Team Daily Standup
+// Daily Team Power Meeting
